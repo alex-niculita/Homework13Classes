d7c7eb3783420aaac0324db878b56f414634e2b3
Sasha <alex_penza@list.ru>
2023-01-31 16:50:34 +0300

    Master created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author author1 = new Author("Mark","Twain");
+        Author author2 = new Author("Jerome David","Salinger");
+        Author author3 = new Author("George","Orwell");
+
+        Book book1 = new Book("Huckleberry Finn", author1, 1884);
+        Book book2 = new Book("Catcher in the Rye", author2, 1951);
+        Book book3 = new Book("Animal Farm", author3, 1945);
+
+        book2.setYear(1832);
+
     }
 }
